0b7edc6756e0de05fbaf59c1ab57bf55a0b3cef7
pec0ra <heraclez@hotmail.com>
2017-08-21 16:33:00 +0200

    Put quick charge configuration in a dedicated file

--- drivers/power/quick_charge_manager.c
+++ drivers/power/quick_charge_manager.c
@@ -14,34 +14,26 @@
  *
  */
 
-// TODO: Does nothing yet as variables need to be imported in qpnp-charger.c
+#include <linux/moduleparam.h>
 
 #include <linux/module.h>
 #include <linux/input.h>
+#include <linux/power_supply.h>
 #include <linux/power/quick_charge_manager.h>
 
 
-static int ibatmax_ma_under_step = IBATMAX_MA_UNDER_STEP_DEFAULT;
-static int qc_enabled = ENABLE_QUICK_CHARGE;
+int ibatmax_ma_under_step = IBATMAX_MA_UNDER_STEP_DEFAULT;
+int qc_enabled = ENABLE_QUICK_CHARGE;
+int pin_code = DEFAULT_PIN_CODE;
 
 static int
 set_ibatmax_ma_under_step(const char *val, const struct kernel_param *kp){
-	int rc = 0;
 	
 	/* Read value from sys */
 	int ret = param_set_int(val, kp);
 
-	struct power_supply *batt_psy = power_supply_get_by_name("battery");
-	struct qpnp_chg_chip *chip = container_of(batt_psy,
-				struct qpnp_chg_chip, batt_psy);
-	if(qc_enabled){
-		printk(TAG "Changing ibatmax_ma_under_step to %d\n", ibatmax_ma_under_step);
-		chip->somc_params.stepchg_ibatmax_ma_under_step = ibatmax_ma_under_step;
-	} else {
-		printk(TAG "QC not enabled. keeping ibatmax_ma_under_step to %d\n", chip->somc_params.stepchg_ibatmax_ma_under_step);
-		if (rc)
-			pr_err("failed to read required dt parameters %d\n", rc);
-	}
+
+        printk(TAG "ibatmax_ma_under_step changed to %d\n", ibatmax_ma_under_step);
 	return ret;
 }
 static struct kernel_param_ops params_ops_change = {
@@ -52,36 +44,24 @@ module_param_cb(ibatmax_ma_under_step, &params_ops_change, &ibatmax_ma_under_ste
 
 static int
 set_qc_enabled(const char *val, const struct kernel_param *kp){
-	int rc = 0;
-	
-	/* Read value from sys */
-	int ret = param_set_bool(val, kp);
 
-	struct power_supply *batt_psy = power_supply_get_by_name("battery");
-	struct qpnp_chg_chip *chip = container_of(batt_psy,
-				struct qpnp_chg_chip, batt_psy);
+        int ret;
+	
+        if(pin_code != CORRECT_PIN_CODE){
+            printk(TAG "Incorrect pin code !");
+            qc_enabled = 0;
+            return 1;
+        }
+
+	/* Read value from sys */
+	ret = param_set_bool(val, kp);
+
 
 	if(qc_enabled){
-		chip->somc_params.stepchg_ibatmax_ma_under_step = ibatmax_ma_under_step;
-		chip->max_bat_chg_current = MAX_BAT_CHG_CURRENT;
-		chip->safe_current = SAFE_CURRENT;
-		chip->somc_params.maxinput_usb_mv = MAXINPUT_MV;
-		chip->somc_params.maxinput_dc_mv = MAXINPUT_MV;
-		printk(TAG "QC 2.0 enabled");
-		printk(TAG "ibatmax_ma_under_step is set to %d\n", ibatmax_ma_under_step);
+            printk(TAG "QC 2.0 enabled");
+            printk(TAG "ibatmax_ma_under_step is set to %d\n", ibatmax_ma_under_step);
 	} else {
-		OF_PROP_READ(chip, safe_current, "ibatsafe-ma", rc, 0);
-		OF_PROP_READ(chip, max_bat_chg_current, "ibatmax-ma", rc, 0);
-	
-		OF_PROP_READ(chip, somc_params.maxinput_dc_mv, "maxinput-dc-mv",
-				rc, 1);
-		OF_PROP_READ(chip, somc_params.maxinput_usb_mv, "maxinput-usb-mv",
-				rc, 1);
-		OF_PROP_READ(chip, somc_params.stepchg_ibatmax_ma_under_step,
-				"ibatmax-ma-under-step", rc, 1);
-		if (rc)
-			pr_err("failed to read required dt parameters %d\n", rc);
-		printk(TAG "QC 2.0 disabled");
+            printk(TAG "QC 2.0 disabled");
 
 	}
 	return ret;
@@ -91,3 +71,19 @@ static struct kernel_param_ops params_ops_enable = {
        .get = param_get_uint,
 };
 module_param_cb(qc_enabled, &params_ops_enable, &qc_enabled, 0644);
+
+static int
+set_pin_code(const char *val, const struct kernel_param *kp){
+	
+	/* Read value from sys */
+	int ret = param_set_int(val, kp);
+
+
+        printk(TAG "Pin set to  %d\n", pin_code);
+	return ret;
+}
+static struct kernel_param_ops params_ops_pin = {
+       .set = set_pin_code,
+       .get = param_get_uint,
+};
+module_param_cb(pin_code, &params_ops_pin, &pin_code, 0644);
--- include/linux/power/quick_charge_manager.h
+++ include/linux/power/quick_charge_manager.h
@@ -18,13 +18,16 @@
 #define _QUICK_CHARGE_MANAGER_H
 
 #define TAG					"QC2.0 : "
-#define ENABLE_QUICK_CHARGE			0
+#define ENABLE_QUICK_CHARGE			1
 #define NO_IBATMAX				-1
 #define MAX_IBATMAX_MA_UNDER_STEP		2150
 #define IBATMAX_MA_UNDER_STEP_DEFAULT		2150
 #define MAX_BAT_CHG_CURRENT			2150
 #define SAFE_CURRENT				2150
 #define MAXINPUT_MV				13000
+
+#define DEFAULT_PIN_CODE                        0
+#define CORRECT_PIN_CODE                        535218
 
 extern int qc_enabled;
 extern int ibatmax_ma_under_step;
